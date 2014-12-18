c0ea8a91d8a4f72fb2570289fb0b0752df345fba
Augusto Cravo Silva <augustocravosilva@gmail.com>
2014-12-18 11:05:46 +0000

    little change

--- FirstREST/FirstREST/Lib_Primavera/Comercial.cs
+++ FirstREST/FirstREST/Lib_Primavera/Comercial.cs
@@ -838,9 +838,7 @@ namespace FirstREST.Lib_Primavera
                         myEnc.set_TipoEntidade("C");
                         myEnc.set_Seccao("2");//vendas a retalho
                         myEnc.set_CondPag("1"); // pronto pagamento
-
-                        myEnc = PriEngine.Engine.Comercial.Vendas.PreencheDadosRelacionados(myEnc, PreencheRelacaoVendas.vdDadosTodos);
-
+                        
                         foreach (Model.LinhaDocVenda lin in dv.lines)
                         {
                             double preco = PriEngine.Engine.Comercial.ArtigosPrecos.ListaArtigosMoedas(lin.product_id)[1].get_PVP1();
@@ -864,6 +862,8 @@ namespace FirstREST.Lib_Primavera
                                 myLinhas.Insere(l);
                             }
                         }
+                        //apos verificar produtos para nao perder tempo aqui se houver falta de stock
+                        myEnc = PriEngine.Engine.Comercial.Vendas.PreencheDadosRelacionados(myEnc, PreencheRelacaoVendas.vdDadosTodos);
 
                         myEnc.set_Linhas(myLinhas);
 
